2e1462a34bfb125f6b48be6aaa3f3c14eb757020
Dmitry Evseev <ew.dim@mail.ru>
2021-10-19 04:51:31 +0700

    Create and connect BurgerIngredients

--- react-burger/src/components/app/app.tsx
+++ react-burger/src/components/app/app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+
+import { data } from '../../utils/data';
 
 function App() {
   return (
     <div>
       <AppHeader />
+      <BurgerIngredients ingredients={data}/>
     </div>
   );
 }
--- react-burger/src/components/burger-ingredients/burger-ingredients.tsx
+++ react-burger/src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function BurgerIngredients() {
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import BurgerIngredientsItems from '../burger-ingredients-item/burger-ingredients-item';
+import stylesBurgerIngredients from './burger-ingredients.module.css';
+
+function BurgerIngredients(props: any) {
+  const tabs = [
+    {
+      name: 'bun',
+      title: 'Булки',
+    },
+    {
+      name: 'sauce',
+      title: 'Соусы',
+    },
+    {
+      name: 'main',
+      title: 'Начинки',
+    },
+  ]
+  
+  const [current, setCurrent] = React.useState(tabs[0].name);
+
   return (
-    <div>
-    </div>
+    <section>
+      <div style={{ display: 'flex' }}>
+        {
+          tabs.map((tab) => (
+            <Tab 
+              onClick={setCurrent} 
+              active={current === tab.name} 
+              value={tab.name} 
+              key={tab.name}
+            >
+              {tab.title}
+            </Tab>
+          ))
+        }
+      </div>
+      <BurgerIngredientsItems
+        data={props.ingredients.filter((el: any) => el.type === current)}
+      />
+    </section>
   );
 }
+
+BurgerIngredients.propTypes = { 
+  ingredients: PropTypes.arrayOf(PropTypes.shape({  
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    proteins: PropTypes.number,
+    fat: PropTypes.number,
+    carbohydrates: PropTypes.number,
+    calories: PropTypes.number,
+    price: PropTypes.number,
+    image: PropTypes.string,
+    image_mobile: PropTypes.string,
+    image_large: PropTypes.string,
+    __v: PropTypes.number
+  }))
+};
 
 export default BurgerIngredients;
