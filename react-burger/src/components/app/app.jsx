a6d630bdc8f47d2634104a32ed6dcec2d41e1ddf
Dmitry Evseev <ew.dim@mail.ru>
2021-11-19 21:06:53 +0700

    replaced app page to HomePage and create link for it

--- react-burger/src/components/app/app.jsx
+++ react-burger/src/components/app/app.jsx
@@ -1,91 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import { useDispatch } from 'react-redux';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import '@ya.praktikum/react-developer-burger-ui-components';
+import { HomePage } from '../../pages';
 
-import stylesApp from './app.module.css';
+import styles from './app.module.css';
 
-import AppHeader from '../app-header/app-header';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-import Modal from '../modal/modal';
-import IngredientDetails from '../ingredient-details/ingredient-details';
-import OrderDetails from '../order-details/order-details';
-
-import { 
-  getIngredients,
-  SET_CURRENT_INGREDIENT,
-  DELETE_CURRENT_INGREDIENT
-} from '../../services/actions/ingredients';
-
-function App() {
-  const dispatch = useDispatch();
-
-  const [isOpenOrder, setIsOpenOrder] = React.useState(false);
-  const [isOpenIngredient, setIsOpenIngredient] = React.useState(false);
-  const [statusLoading, setStatusLoading] = useState({ 
-    isLoading: true,
-    isError: false
-  });
-
-  useEffect(() => {
-    setStatusLoading({ isLoading: true, isError: false });
-    try {
-      dispatch(getIngredients());
-      setStatusLoading({ isLoading: false, isError: false });
-    } catch (error) {
-      setStatusLoading({ isLoading: false, isError: true });
-    }
-  }, [dispatch]);
-
-  const openIngredientModal = (item) => {
-    dispatch({type: SET_CURRENT_INGREDIENT, currentIngredient: item});
-    setIsOpenIngredient(true);
-  };
-
-  const openOrderModal = () => {
-    setIsOpenOrder(true);
-  };
-
-  const closeModal = () => {
-    setIsOpenIngredient(false);
-    setIsOpenOrder(false);
-    dispatch({type: DELETE_CURRENT_INGREDIENT});
-  };
-
+export default function App() {
   return (
-    <div className={stylesApp.root}>
-      <AppHeader />
-      {statusLoading.isLoading && !statusLoading.isError && <div className={`${stylesApp.loader} text_type_main-medium`}>Загрузка...</div>}
-      {statusLoading.isError && !statusLoading.isLoading && <div className={`${stylesApp.loader} text_type_main-medium`}>Упс! Что-то пошло не так =(</div>}
-      {
-        !statusLoading.isLoading && 
-        !statusLoading.isError &&
-        <div className={stylesApp.container}>
-          <DndProvider backend={HTML5Backend}> 
-            <BurgerIngredients openModal={openIngredientModal}/>
-            <BurgerConstructor openModal={openOrderModal}/>
-          </DndProvider>
-          { isOpenIngredient && 
-            (
-                <Modal onClick={closeModal} header="Детали ингредиента">
-                  <IngredientDetails />
-                </Modal>
-            )
-          }
-          { isOpenOrder && 
-            (
-                <Modal onClick={closeModal} header="">
-                  <OrderDetails />
-                </Modal>
-            )
-          }
-        </div>
-      }
-    </div>
-  );
-}
-
-export default App;
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
