e7c3adb990e80c62f23a4576fae47b265fc32de3
Dmitry Evseev <ew.dim@mail.ru>
2021-11-24 05:11:48 +0700

    Reworked thunk func as action func for Modal

--- react-burger/src/services/actions/modal.js
+++ react-burger/src/services/actions/modal.js
@@ -9,21 +9,19 @@ export const IS_OPEN_INGREDIENT = 'IS_OPEN_INGREDIENT';
 
 
 export function openOrderModal() {
-    return (dispatch) => { dispatch({ type: IS_OPEN_ORDER, isOpen: true }) };
+    return { type: IS_OPEN_ORDER, isOpen: true };
 }
 
 export function closeOrderModal() {
-    return (dispatch) => { dispatch({ type: IS_OPEN_ORDER, isOpen: false }) };
+    return { type: IS_OPEN_ORDER, isOpen: false };
 }
 
 export function openIngredientModal() {
     if (!getCookie('isOpenIngredientModal')) setCookie('isOpenIngredientModal', true);
-    return (dispatch) => { dispatch({ type: IS_OPEN_INGREDIENT, isOpen: true }) };
+    return { type: IS_OPEN_INGREDIENT, isOpen: true };
 }
 
 export function closeIngredientModal() {
     deleteCookie('isOpenIngredientModal', { path: '/' });
-    return (dispatch) => {
-        dispatch({ type: IS_OPEN_INGREDIENT, isOpen: false });
-    };
+    return { type: IS_OPEN_INGREDIENT, isOpen: false };
 }
